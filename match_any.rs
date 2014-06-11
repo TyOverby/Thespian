19fb558728f11abe1d3a37849f030b9810b7746b
TyOverby <ty@pre-alpha.com>
2014-06-10 22:44:39 -0700

    initial commit

---
+++ match_any.rs
@@ -0,0 +1,19 @@
+// Copyright 2014: Ty Overby
+
+#[macro_export]
+macro_rules! match_any(
+    ($val:expr match  $(if $typ:ty { $($patn: pat => $exp: expr),+  }),+ else { $other: expr}) => (
+        {
+            $(
+                if $val.is::<$typ>() {
+                    match $val.as_ref::<$typ>().unwrap() {
+                        $($patn => $exp),*
+                    }
+                } else
+            )*
+            {
+                $other
+            }
+        }
+    )
+)
